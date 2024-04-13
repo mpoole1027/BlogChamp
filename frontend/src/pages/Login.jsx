aba264c0d2e874476e02479a48cd5f4c3e4dc74b
Kane Davidson <kaned2001@icloud.com>
2024-04-13 14:59:12 -0700

    Fixed sign up tables

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,6 +5,10 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
   const navigate = useNavigate();
 
   const handleLogin = () => {
@@ -32,8 +36,8 @@ const Login = () => {
       <button onClick={handleLogin}>Login</button>
 
       <h2>Sign Up</h2>
-      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <input type="text" placeholder="newUsername" value={username} onChange={(e) => setNewUsername(e.target.value)} />
+      <input type="password" placeholder="newPassword" value={password} onChange={(e) => setNewPassword(e.target.value)} />
       <button onClick={handleLogin}>Login</button>
     </div>
 
