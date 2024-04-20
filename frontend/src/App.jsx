2d39053edfafd1af36d784ca9a8ee704268c57bf
Josh VanderMeer <Lobojoshvandermeer@gmail.com>
2024-04-20 11:00:27 -0700

    profile functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Blog from './pages/Blog';
 import Post from './pages/Post';
 import Profile from './pages/Profile';
 
-
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          {/* Routes */}
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/post" element={<Post />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
       </Router>
     </div>
   );
