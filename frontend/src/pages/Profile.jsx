577f6051621fb73066a5fba2ae65f962612738c5
IzMo2000 <izaac.6.30@gmail.com>
2024-04-24 15:14:14 -0700

    added an edit bio option

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,8 +6,10 @@ import './Profile.css';
 
 
 const Profile = () => {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null)
+  const [error, setError] = useState(null)
+  const [editingBio, setEditingBio] = useState(false)
+  const [newBio, setNewBio] = useState('')
   const { username } = useParams(); // Get username from URL parameter
 
   useEffect(() => {
@@ -27,6 +29,21 @@ const Profile = () => {
     fetchUser();
   }, []);
 
+  const handleEditBio = () => {
+    setEditingBio(!editingBio)
+    setNewBio(user.bio)
+  }
+
+  const handleSaveBio = () => {
+    setUser({...user, bio: newBio})
+    UserFacade.updateUser({ ...user, bio: newBio })
+    setEditingBio(false)
+  }
+
+  const handleChangeBio = (event) => {
+    setNewBio(event.target.value)
+  }
+
   return (
     <div className="profile">
       <div className="sidebar">
@@ -34,13 +51,24 @@ const Profile = () => {
       </div>
       <div className="main-content">
         <h1 className="profile-title">Profile</h1>
-        {user && (
+         {user && (
           <div className="profile-info">
             <h2>Username: {user.username}</h2>
-            <div className="bio-box">
-              <h3>Bio</h3>
-              <p>{user.bio}</p>
-            </div>
+            {editingBio ? (
+              // Textbox for editing bio
+              <div className="bio-box">
+                <h3>Edit Bio</h3>
+                <textarea value={newBio} onChange={handleChangeBio} />
+                <button onClick={handleSaveBio}>Save Bio</button>
+              </div>
+            ) : (
+              // Display bio as plain text
+              <div className="bio-box">
+                <h3>Bio</h3>
+                <p>{user.bio}</p>
+                <button onClick={handleEditBio}>Edit Bio</button>
+              </div>
+            )}
           </div>
         )}
         {error && <p>Error: {error}</p>}
